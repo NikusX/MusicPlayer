18ca54fb9186ee8d663b01c584116f83d7d6da06
NikusX <tigrnik3@gmail.com>
2017-09-28 22:10:09 +0300

    Make the first step

---
+++ MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.nextTrackButton = new System.Windows.Forms.Button();
+            this.previousTrackButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Сейчас играет:";
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.playButton.Location = new System.Drawing.Point(254, 276);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(115, 50);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pauseButton.Location = new System.Drawing.Point(133, 276);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(115, 50);
+            this.pauseButton.TabIndex = 2;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.Location = new System.Drawing.Point(375, 276);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(115, 50);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // nextTrackButton
+            // 
+            this.nextTrackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nextTrackButton.Location = new System.Drawing.Point(496, 276);
+            this.nextTrackButton.Name = "nextTrackButton";
+            this.nextTrackButton.Size = new System.Drawing.Size(115, 50);
+            this.nextTrackButton.TabIndex = 4;
+            this.nextTrackButton.Text = "Next";
+            this.nextTrackButton.UseVisualStyleBackColor = true;
+            this.nextTrackButton.Click += new System.EventHandler(this.nextTrackButton_Click);
+            // 
+            // previousTrackButton
+            // 
+            this.previousTrackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.previousTrackButton.Location = new System.Drawing.Point(12, 276);
+            this.previousTrackButton.Name = "previousTrackButton";
+            this.previousTrackButton.Size = new System.Drawing.Size(115, 50);
+            this.previousTrackButton.TabIndex = 5;
+            this.previousTrackButton.Text = "Previous";
+            this.previousTrackButton.UseVisualStyleBackColor = true;
+            this.previousTrackButton.Click += new System.EventHandler(this.previousTrackButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackName
+            // 
+            this.trackName.AutoSize = true;
+            this.trackName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.trackName.Location = new System.Drawing.Point(170, 9);
+            this.trackName.Name = "trackName";
+            this.trackName.Size = new System.Drawing.Size(156, 25);
+            this.trackName.TabIndex = 6;
+            this.trackName.Text = "название трека";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 338);
+            this.Controls.Add(this.trackName);
+            this.Controls.Add(this.previousTrackButton);
+            this.Controls.Add(this.nextTrackButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Музыкальный плеер";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button nextTrackButton;
+        private System.Windows.Forms.Button previousTrackButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label trackName;
+    }
+}
+
---
+++ MusicPlayer/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player.URL = "Imagine Dragons – Thunder.mp3";
+            player.controls.stop();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            player.controls.play();
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            player.controls.pause();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            player.controls.stop();
+        }
+
+        private void previousTrackButton_Click(object sender, EventArgs e)
+        {
+            player.controls.previous();
+        }
+
+        private void nextTrackButton_Click(object sender, EventArgs e)
+        {
+            player.controls.next();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            trackName.Text = player.currentMedia.name;
+        }
+    }
+}
